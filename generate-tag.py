757fac2a75a2b3f5c720e919c56237fb5abcf385
mattedamo <mattedamo@gmail.com>
2021-01-29 11:57:11 +0100

    a

--- generate-tag.py
+++ generate-tag.py
@@ -1,10 +1,11 @@
-import sys
+import sys, os
 
 def main():
     #devo fare una versione sia per l'hotfix, sia una per la release, qua le metto tutte e 2
     #differenziando i due flow
     #type_branch = "hotfix"
     #print(sys.argv)
+    this_branch = os.environ["GITHUB_HEAD_REF"]
     list = (sys.argv)
     del list[0]
     if len(list) == 0:
@@ -13,15 +14,16 @@ def main():
         list.sort()
         last_tag = list[-1]
         last_tag_list = last_tag.split(".")
-        """if(type_branch == "hotfix"):
+        if("releases" in this_branch):
+            #nuova release
+            value_to_update = int(last_tag_list[0][1:])
+            value_to_update += 1
+            new_tag = "v"+str(value_to_update)+".0"
+        else:
+            #hotfix    
             value_to_update = int(last_tag_list[1])
             value_to_update += 1
             new_tag = last_tag_list[0]+"."+str(value_to_update)
-        else:"""
-        #nuova release
-        value_to_update = int(last_tag_list[0][1:])
-        value_to_update += 1
-        new_tag = "v"+str(value_to_update)+".0"
     print(new_tag)
     
 
